91b65e6da01c2d547b2e598ab49a8e65b9457bdd
LAROUI Nacim <l.nassimo@gmail.com>
2020-06-10 15:46:05 +0200

    flexing items to get the layout ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './UI/footer/footer.component';
 import { HeaderComponent } from './UI/header/header.component';
 import { MenuCardComponent } from './menu-card/menu-card.component';
+import { ContainerComponent } from './container/container.component';
+import { LegalnoticeComponent } from './legalnotice/legalnotice.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     HeaderComponent,
-    MenuCardComponent
+    MenuCardComponent,
+    ContainerComponent,
+    LegalnoticeComponent
   ],
   imports: [
     BrowserModule,
